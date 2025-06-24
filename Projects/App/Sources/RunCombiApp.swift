0a76b5c44e95af59d1ad4f968873d1b4d4734b8a
rudqlsdla <hsmikvyv@gmail.com>
2025-06-25 03:13:42 +0900

    feat($tuist): FeatureMain 추가

--- Projects/App/Sources/RunCombiApp.swift
+++ Projects/App/Sources/RunCombiApp.swift
@@ -1,10 +1,20 @@
+//
+//  RunCombiApp.swift
+//  RunCombi
+//
+//  Created by 임경빈 on 6/25/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
 import SwiftUI
+
+import FeatureMain
 
 @main
 struct RunCombiApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      MainView()
     }
+  }
 }
