0556d1c40b7f8ed4eb61f243c0c36884bed6737e
Gyeongbin Im <hsmikvyv@gmail.com>
2025-06-26 21:13:33 +0900

    feat($tuist): CoreNetwork 추가

--- Projects/Core/Network/Project.swift
+++ Projects/Core/Network/Project.swift
@@ -1,24 +1,29 @@
+//
+//  Project.swift
+//  AppManifests
+//
+//  Created by 임경빈 on 6/26/25.
+//
+
 import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.make(
-  name: "FeatureMain",
+  name: "CoreNetwork",
   targets: [
     .make(
-      name: "FeatureMain",
+      name: "CoreNetwork",
       product: .staticLibrary,
-      bundleId: "com.Combo.FeatureMain",
+      bundleId: "com.Combo.CoreNetwork",
       sources: ["Sources/**"],
       dependencies: [
+        .external(name: "Alamofire"),
         .project(
           target: "SharedUtility",
           path: .relativeToRoot("Projects/Shared/Utility")
         ),
-        .project(
-          target: "CoreNetwork",
-          path: .relativeToRoot("Projects/Core/Network")
-        )
       ]
     )
   ]
 )
+
