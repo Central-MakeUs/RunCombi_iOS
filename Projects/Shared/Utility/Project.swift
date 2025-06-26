9792478201bdbfc647058e087b78a5e2e33eed98
Gyeongbin Im <hsmikvyv@gmail.com>
2025-06-26 21:00:31 +0900

    feat($tuist): SharedUtility 추가

---
+++ Projects/Shared/Utility/Project.swift
@@ -0,0 +1,24 @@
+//
+//  Project.swift
+//  AppManifests
+//
+//  Created by 임경빈 on 6/26/25.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.make(
+  name: "SharedUtility",
+  targets: [
+    .make(
+      name: "SharedUtility",
+      product: .framework,
+      bundleId: "com.Combo.SharedUtility",
+      sources: ["Sources/**"],
+      dependencies: [
+        
+      ]
+    )
+  ]
+)
