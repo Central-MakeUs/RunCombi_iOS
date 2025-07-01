dc960058c17a049636e03527a6a0f408e5912b68
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-02 00:34:35 +0900

    feat($tuist): FeatureLogin 추가

---
+++ Projects/Feature/Login/Project.swift
@@ -0,0 +1,24 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.make(
+  name: "FeatureLogin",
+  targets: [
+    .make(
+      name: "FeatureLogin",
+      product: .staticLibrary,
+      bundleId: "com.Combo.FeatureLogin",
+      sources: ["Sources/**"],
+      dependencies: [
+        .project(
+          target: "SharedUtility",
+          path: .relativeToRoot("Projects/Shared/Utility")
+        ),
+        .project(
+          target: "UserInterface",
+          path: .relativeToRoot("Projects/DesignSystem/UserInterface")
+        )
+      ]
+    )
+  ]
+)
