8b6e3b14ec613b89e02f08fe48e5a927f0812f50
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-03 01:36:08 +0900

    feat($user): 유저 정보 입력 화면 분기 처리

---
+++ Projects/Feature/SignUp/Sources/Views/UserInfo/GenderInputView.swift
@@ -0,0 +1,19 @@
+//
+//  GenderInputView.swift
+//  FeatureSignUp
+//
+//  Created by 임경빈 on 7/3/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
+import SwiftUI
+
+struct GenderInputView: View {
+    var body: some View {
+        Text("성별 입력")
+    }
+}
+
+#Preview {
+    GenderInputView()
+}
