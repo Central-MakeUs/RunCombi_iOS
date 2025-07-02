a5d35e26d1913a923fa3d4661d40f015b84df846
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-03 01:36:08 +0900

    feat($user): 유저 정보 입력 화면 분기 처리

---
+++ Projects/Feature/SignUp/Sources/Views/UserInfo/NicknameInputView.swift
@@ -0,0 +1,19 @@
+//
+//  NicknameInputView.swift
+//  FeatureSignUp
+//
+//  Created by 임경빈 on 7/3/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
+import SwiftUI
+
+struct NicknameInputView: View {
+    var body: some View {
+        Text("닉네임 입력")
+    }
+}
+
+#Preview {
+    NicknameInputView()
+}
