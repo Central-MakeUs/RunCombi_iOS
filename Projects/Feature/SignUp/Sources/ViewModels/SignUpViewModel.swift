08e9195c87ab04b0f5f9da20ebf87a197f88170c
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-02 21:19:56 +0900

    feat($signUp): SignUpViewModel 구현

---
+++ Projects/Feature/SignUp/Sources/ViewModels/SignUpViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  SignUpViewModel.swift
+//  FeatureSignUp
+//
+//  Created by 임경빈 on 7/2/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
+import Foundation
+
+import SharedUtility
+
+class SignUpViewModel: ViewModelable {
+  
+  // MARK: - Actions
+  
+  enum Action {
+    
+  }
+  
+  // MARK: - States
+  
+  struct State {
+    
+  }
+  
+  // MARK: - Properties
+  
+  @Published var state = State()
+  
+  // MARK: - Initialize
+  
+  init() {
+    
+  }
+  
+  // MARK: - Action
+  
+  func send(action: Action) {
+    switch action {
+      
+    }
+  }
+}
