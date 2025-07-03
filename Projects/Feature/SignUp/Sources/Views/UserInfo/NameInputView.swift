3afa1ae45f0fee802259ae80fec43ef437d4fc97
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-04 00:53:30 +0900

    feat($name): 이름 입력 화면 UI 구현

---
+++ Projects/Feature/SignUp/Sources/Views/UserInfo/NameInputView.swift
@@ -0,0 +1,71 @@
+//
+//  NameInputView.swift
+//  FeatureSignUp
+//
+//  Created by 임경빈 on 7/3/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
+import SwiftUI
+
+import ResourceKit
+import UserInterface
+
+struct NameInputView: View {
+  @ObservedObject var viewModel: SignUpViewModel
+  @State private var typpedNickname: String = ""
+  
+  init(of viewModel: SignUpViewModel) {
+    self.viewModel = viewModel
+  }
+  
+  var body: some View {
+    VStack(spacing: 26) {
+      Button {
+        
+      } label: {
+        Image(R.image.person)
+          .overlay(alignment: .bottomTrailing) {
+            Image(R.image.camera)
+              .padding(.bottom, 9)
+          }
+      }
+      
+      VStack(spacing: 5) {
+        HStack {
+          Text("이름")
+            .pretendardFont(size: 12, weight: .semiBold, lineHeight: 24)
+            .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
+          Spacer()
+          Text("한글 6자/ 영문 10자 이하")
+            .pretendardFont(size: 12, weight: .medium, lineHeight: 24)
+            .foregroundStyle(Color(R.color.greyscale_06_999999))
+        }
+        
+        TextField(
+          "",
+          text: $typpedNickname,
+          prompt: Text("런콤비")
+            .foregroundStyle(Color(R.color.greyscale_06_999999))
+        )
+        .pretendardFont(size: 12, weight: .semiBold, lineHeight: 24)
+        .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
+        .padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
+        .background(Color(R.color.greyscale_04_525252))
+        .clipShape(.rect(cornerRadius: 6))
+      }
+      
+      Spacer()
+      
+      Button {
+        viewModel.state.userInfoInputType = .gender
+      } label: {
+        PrimaryActionLabel(
+          text: String(key: "Common.Next"),
+          backgroundColor: typpedNickname.isEmpty ? Color(R.color.gray_353434) : Color(R.color.primary_01_D7FE63)
+        )
+      }
+      .disabled(typpedNickname.isEmpty)
+    }
+  }
+}
