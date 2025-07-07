787adb80e9a88a8c2c3e26bd4dd6e4a4f5ec4320
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-08 01:22:59 +0900

    feat($exercise): FeatureExercise 추가

---
+++ Projects/Feature/Exercise/Project.swift
@@ -0,0 +1,25 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.make(
+  name: "FeatureExercise",
+  targets: [
+    .make(
+      name: "FeatureExercise",
+      product: .staticLibrary,
+      bundleId: "com.Combo.FeatureExercise",
+      sources: ["Sources/**"],
+      dependencies: [
+        .external(name: "GoogleMaps"),
+        .project(
+          target: "SharedUtility",
+          path: .relativeToRoot("Projects/Shared/Utility")
+        ),
+        .project(
+          target: "UserInterface",
+          path: .relativeToRoot("Projects/DesignSystem/UserInterface")
+        )
+      ]
+    )
+  ]
+)
