ea7ecd588e29098115a9e7f73de320a336ae3ce1
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-11 00:18:28 +0900

    feat($main): 메인 Tab 구현

---
+++ Projects/Feature/Main/Sources/Utils/MainTab.swift
@@ -0,0 +1,40 @@
+//
+//  MainTab.swift
+//  FeatureMain
+//
+//  Created by 임경빈 on 7/10/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
+import SwiftUI
+import ResourceKit
+
+enum MainTab: CaseIterable {
+  case calendar
+  case exercise
+  case myPage
+}
+
+extension MainTab {
+  var icon: Image {
+    switch self {
+    case .calendar:
+      Image(R.image.calendar)
+    case .exercise:
+      Image(R.image.exercise)
+    case .myPage:
+      Image(R.image.myPage)
+    }
+  }
+  
+  var selectedIcon: Image {
+    switch self {
+    case .calendar:
+      Image(R.image.selectedCalendar)
+    case .exercise:
+      Image(R.image.selectedExercise)
+    case .myPage:
+      Image(R.image.selectedMyPage)
+    }
+  }
+}
