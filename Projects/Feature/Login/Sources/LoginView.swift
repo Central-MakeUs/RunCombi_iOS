ea28d6550b51708fee4723269a90393efedaf37a
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-12 00:37:15 +0900

    feat($login): 로그인 버튼 UI 구현

--- Projects/Feature/Login/Sources/LoginView.swift
+++ Projects/Feature/Login/Sources/LoginView.swift
@@ -35,13 +35,47 @@ public struct LoginView: View {
           Button {
             // TODO: = 카카오 Login
           } label: {
-            Text("카카오로 시작하기")
+            ZStack {
+              HStack {
+                Image(R.image.symbolKakao)
+                Spacer()
+              }
+              .padding(.leading, 23)
+              
+              HStack {
+                Spacer()
+                Text("카카오로 시작하기")
+                  .pretendardFont(size: 16, weight: .medium, lineHeight: 25)
+                  .foregroundStyle(Color(R.color.black_000000))
+                Spacer()
+              }
+            }
+            .frame(height: 48)
+            .background(Color(R.color.yellow_FEE102))
+            .clipShape(.rect(cornerRadius: 6))
           }
           
           Button {
             // TODO: = Apple Login
           } label: {
-            Text("Apple로 시작하기")
+            ZStack {
+              HStack {
+                Image(R.image.symbolApple)
+                Spacer()
+              }
+              .padding(.leading, 23)
+              
+              HStack {
+                Spacer()
+                Text("Apple로 시작하기")
+                  .pretendardFont(size: 16, weight: .medium, lineHeight: 25)
+                  .foregroundStyle(Color(R.color.black_000000))
+                Spacer()
+              }
+            }
+            .frame(height: 48)
+            .background(Color(R.color.white_FFFFFF))
+            .clipShape(.rect(cornerRadius: 6))
           }
           
           // 임시 버튼
@@ -51,6 +85,7 @@ public struct LoginView: View {
             Text("임시 버튼")
           }
         }
+        .padding(.horizontal, 20)
       }
       .frame(maxWidth: .infinity)
       .background(Color(R.color.greyscale_01_171717))
