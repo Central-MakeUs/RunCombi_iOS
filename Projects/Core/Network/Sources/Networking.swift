04ddd30ae2c76617a0bc4f0f211f23bcec64b3bb
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-12 03:11:09 +0900

    feat($network): 네트워킹 Manager 구현

---
+++ Projects/Core/Network/Sources/Networking.swift
@@ -0,0 +1,69 @@
+//
+//  Networking.swift
+//  CoreNetwork
+//
+//  Created by 임경빈 on 7/12/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
+import Foundation
+
+import Alamofire
+import SharedUtility
+
+public final class Networking {
+  public static let shared = Networking()
+  private let baseURL = "http://api.runcombi.site"
+  
+  // MARK: Initialize
+  
+  private init() {}
+  
+  // MARK: Requests
+  
+  public func sendRequest<T: Decodable>(
+    _ url: String,
+    resultType: T.Type,
+    method: HTTPMethod,
+    parameters: Parameters? = nil,
+    encoding: ParameterEncoding = URLEncoding.default
+  ) async throws -> T {
+    if !NetworkMonitor.shared.isConnected {
+      throw NetworkError.disconected
+    }
+    
+    return try await AF.request(
+      baseURL.appending(url),
+      method: method,
+      parameters: parameters,
+      encoding: encoding
+    )
+    .validate()
+    .serializingDecodable()
+    .value
+  }
+  
+  public func sendRequestWithRaw<T: Decodable>(
+    _ url: String,
+    resultType: T.Type,
+    method: HTTPMethod,
+    rawBody: Data? = nil
+  ) async throws -> T {
+    if !NetworkMonitor.shared.isConnected {
+      throw NetworkError.disconected
+    }
+    
+    var urlRequest = try URLRequest(url: baseURL + url, method: method)
+    
+    urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    if let body = rawBody {
+      urlRequest.httpBody = body
+    }
+    
+    return try await AF.request(urlRequest)
+      .validate()
+      .serializingDecodable(resultType)
+      .value
+  }
+}
