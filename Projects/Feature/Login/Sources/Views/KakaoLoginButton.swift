f2fd1c9c3992c2d3b5ea685eb644cbe5e06e647f
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-12 01:22:45 +0900

    refactor($login): 로그인 버튼 view 분리

---
+++ Projects/Feature/Login/Sources/Views/KakaoLoginButton.swift
@@ -0,0 +1,40 @@
+//
+//  KakaoLoginButton.swift
+//  FeatureLogin
+//
+//  Created by 임경빈 on 7/12/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
+import SwiftUI
+
+import ResourceKit
+
+struct KakaoLoginButton: View {
+  let kakaoLoginAction: () -> Void
+  
+  var body: some View {
+    Button {
+      kakaoLoginAction()
+    } label: {
+      ZStack {
+        HStack {
+          Image(R.image.symbolKakao)
+          Spacer()
+        }
+        .padding(.leading, 23)
+        
+        HStack {
+          Spacer()
+          Text("카카오로 시작하기")
+            .pretendardFont(size: 16, weight: .medium, lineHeight: 25)
+            .foregroundStyle(Color(R.color.black_000000))
+          Spacer()
+        }
+      }
+      .frame(height: 48)
+      .background(Color(R.color.yellow_FEE102))
+      .clipShape(.rect(cornerRadius: 6))
+    }
+  }
+}
