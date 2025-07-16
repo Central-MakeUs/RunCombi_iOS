03b96b3da001a7c5cec667b734484082cb76f48a
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-17 00:51:10 +0900

    feat($map): 구글맵에 내 위치를 나타내는 location manager 연결

--- Projects/Feature/Exercise/Sources/Views/GoogleMapView.swift
+++ Projects/Feature/Exercise/Sources/Views/GoogleMapView.swift
@@ -12,17 +12,23 @@ import GoogleMaps
 import SharedUtility
 
 struct GoogleMapView: UIViewRepresentable {
+  private let locationManager = CLLocationManager()
   private let mapView = GMSMapView()
   
   public func makeUIView(context: Context) -> GMSMapView {
     let camera = GMSCameraPosition(latitude: 37.5665, longitude: 126.9780, zoom: 6.5)
     mapView.camera = camera
     setMapStyle()
+    setLocationManager(context)
     return mapView
   }
   
   public func updateUIView(_ uiViewController: GMSMapView, context: Context) {
     
+  }
+  
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
   }
 }
 
@@ -39,5 +45,34 @@ private extension GoogleMapView {
     } catch {
       Logger.e("One or more of the map styles failed to load. \(error)")
     }
+  }
+  
+  func setLocationManager(_ context: Context) {
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    locationManager.requestWhenInUseAuthorization()
+    locationManager.startUpdatingLocation()
+    locationManager.delegate = context.coordinator
+    mapView.isMyLocationEnabled = true
+  }
+}
+
+extension GoogleMapView {
+  final class Coordinator: NSObject {
+    let parent: GoogleMapView
+    
+    init(_ parent: GoogleMapView) {
+      self.parent = parent
+    }
+  }
+}
+
+extension GoogleMapView.Coordinator: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let location: CLLocation = locations.last!
+    Logger.d("Location: \(location)")
+    
+    let camera = GMSCameraPosition.camera(withTarget: location.coordinate, zoom: 15)
+    
+    parent.mapView.animate(to: camera)
   }
 }
