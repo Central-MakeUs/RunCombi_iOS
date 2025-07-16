bd362b4a286a66e16e99addb1374afe420a20007
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-16 23:36:14 +0900

    feat($map): 구글맵 padding 설정

--- Projects/Feature/Exercise/Sources/Views/GoogleMapView.swift
+++ Projects/Feature/Exercise/Sources/Views/GoogleMapView.swift
@@ -1,8 +1,8 @@
 //
-//  ExerciseView.swift
+//  GoogleMapView.swift
 //  FeatureExercise
 //
-//  Created by 임경빈 on 7/8/25.
+//  Created by 임경빈 on 7/16/25.
 //  Copyright © 2025 com.combo. All rights reserved.
 //
 
@@ -10,22 +10,7 @@ import SwiftUI
 
 import GoogleMaps
 
-public struct ExerciseView: View {
-  
-  public init() {}
-  
-  public var body: some View {
-    GMSMapViewRepresentable()
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
-      .background(.red)
-  }
-}
-
-#Preview {
-  ExerciseView()
-}
-
-struct GMSMapViewRepresentable: UIViewRepresentable {
+struct GoogleMapView: UIViewRepresentable {
   
   public func makeUIView(context: Context) -> GMSMapView {
     let camera = GMSCameraPosition(latitude: 47.0169, longitude: -122.336471, zoom: 3)
