f51e03221dd3d025700a0b2c9b3d94a5a5026411
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-17 00:24:38 +0900

    feat($map): 구글맵 다크모드 스타일 적용

--- Projects/Feature/Exercise/Sources/Views/GoogleMapView.swift
+++ Projects/Feature/Exercise/Sources/Views/GoogleMapView.swift
@@ -9,19 +9,35 @@
 import SwiftUI
 
 import GoogleMaps
+import SharedUtility
 
 struct GoogleMapView: UIViewRepresentable {
-  
   public func makeUIView(context: Context) -> GMSMapView {
     let camera = GMSCameraPosition(latitude: 47.0169, longitude: -122.336471, zoom: 3)
-    let mapID = GMSMapID(identifier: "")
     let option = GMSMapViewOptions()
-    option.mapID = mapID
     option.camera = camera
-    return GMSMapView(options: option)
+    let mapView = GMSMapView(options: option)
+    setMapStyle(of: mapView)
+    return mapView
   }
   
   public func updateUIView(_ uiViewController: GMSMapView, context: Context) {
     
+  }
+}
+
+// MARK: - GoogleMap Settings
+
+private extension GoogleMapView {
+  func setMapStyle(of mapView: GMSMapView) {
+    do {
+      if let styleURL = Bundle.main.url(forResource: "dark_style", withExtension: "json") {
+        mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+      } else {
+        Logger.e("Unable to find style.json")
+      }
+    } catch {
+      Logger.e("One or more of the map styles failed to load. \(error)")
+    }
   }
 }
