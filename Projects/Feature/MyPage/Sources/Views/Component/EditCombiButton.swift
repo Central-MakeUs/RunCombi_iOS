6a81edbbe1bbfdffc842bfb96b6ab959b5ec939a
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-18 02:06:46 +0900

    fix($combi): 변경된 콤비 버튼 UI 적용

--- Projects/Feature/MyPage/Sources/Views/Component/EditCombiButton.swift
+++ Projects/Feature/MyPage/Sources/Views/Component/EditCombiButton.swift
@@ -15,21 +15,16 @@ struct EditCombiButton: View {
   let action: () -> Void
   
   var body: some View {
-    VStack(spacing: 8) {
+    VStack(spacing: 12) {
       Image(R.image.defaultDog)
-      VStack(spacing: .zero) {
-        Text("초코")
-          .pretendardFont(size: 18, weight: .semiBold, lineHeight: 30)
-          .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
-        Text("말티푸")
-          .pretendardFont(size: 14, weight: .medium, lineHeight: 24)
-          .foregroundStyle(Color(R.color.greyscale_07_B3B3B3))
-      }
+      Text("초코")
+        .pretendardFont(size: 18, weight: .semiBold, lineHeight: 30)
+        .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
     }
-    .frame(maxWidth: .infinity)
+    .padding(EdgeInsets(top: 35, leading: 0, bottom: 19, trailing: 0))
+    .frame(maxWidth: 154, maxHeight: 154, alignment: .top)
     .aspectRatio(1, contentMode: .fit)
-    .padding(EdgeInsets(top: 26, leading: 0, bottom: 12, trailing: 0))
-    .background(Color(R.color.greyscale_02_252525).opacity(0.3))
+    .background(Color(R.color.greyscale_02_252525))
     .clipShape(.rect(cornerRadius: 6))
     .overlay(alignment: .topTrailing) {
       Button {
