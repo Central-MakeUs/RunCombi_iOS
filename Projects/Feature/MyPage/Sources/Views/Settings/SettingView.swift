856ce5065bd68d256e620b6d21cd80b2f86ce3fd
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-18 02:30:54 +0900

    feat($setting): 설정 화면 UI 구현

--- Projects/Feature/MyPage/Sources/Views/Settings/SettingView.swift
+++ Projects/Feature/MyPage/Sources/Views/Settings/SettingView.swift
@@ -12,8 +12,105 @@ import ResourceKit
 import UserInterface
 
 struct SettingView: View {
+  @Environment(\.dismiss) var dismiss
+  
   var body: some View {
-    Text("설정 화면")
+    VStack(spacing: 16) {
+      ZStack {
+        HStack {
+          Button {
+            dismiss()
+          } label: {
+            Image(R.image.backButton)
+          }
+          Spacer()
+        }
+        
+        HStack {
+          Spacer()
+          Text("설정")
+            .pretendardFont(size: 20, weight: .semiBold, lineHeight: 32)
+            .foregroundStyle(Color(R.color.white_FFFFFF))
+          Spacer()
+        }
+      }
+      .padding(.top, 16)
+      
+      VStack(spacing: 40) {
+        VStack(alignment: .leading, spacing: 16) {
+          Text("약관 및 정책")
+            .pretendardFont(size: 14, weight: .semiBold, lineHeight: 26)
+            .foregroundStyle(Color(R.color.greyscale_05_757575))
+          
+          VStack(spacing: 20) {
+            SettingItem(title: "서비스 이용약관") {
+              
+            }
+            SettingItem(title: "개인정보 처리방침") {
+              
+            }
+            SettingItem(title: "위치정보 이용약관") {
+              
+            }
+          }
+        }
+        
+        VStack(alignment: .leading, spacing: 16) {
+          Text("계정")
+            .pretendardFont(size: 14, weight: .semiBold, lineHeight: 26)
+            .foregroundStyle(Color(R.color.greyscale_05_757575))
+          
+          VStack(spacing: 20) {
+            SettingItem(title: "SNS 로그인") {
+              
+            }
+            SettingItem(title: "로그아웃") {
+              
+            }
+            SettingItem(title: "회원 탈퇴") {
+              
+            }
+          }
+        }
+        
+        VStack(alignment: .leading, spacing: 16) {
+          Text("고객")
+            .pretendardFont(size: 14, weight: .semiBold, lineHeight: 26)
+            .foregroundStyle(Color(R.color.greyscale_05_757575))
+          
+          SettingItem(title: "런콤비 개선 제안") {
+            
+          }
+        }
+      }
+      .padding(.top, 20)
+      .frame(maxWidth: .infinity)
+      
+      Spacer()
+    }
+    .padding(.horizontal, 20)
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(Color(R.color.greyscale_01_171717).ignoresSafeArea())
+    .navigationBarBackButtonHidden()
+  }
+  
+  private struct SettingItem: View {
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+      Button {
+        action()
+      } label: {
+        HStack {
+          Text(title)
+            .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+            .foregroundStyle(Color(R.color.ff_F4F4F4))
+          Spacer()
+          Image(R.image.pushButton)
+        }
+      }
+    }
   }
 }
 
