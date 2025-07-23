5efb41c778a718bd971d74ed6fc8dd2f056b4947
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-23 20:59:38 +0900

    feat($exercise): 운동 카운트다운 화면 구현

--- Projects/Feature/Exercise/Sources/Views/ExerciseView.swift
+++ Projects/Feature/Exercise/Sources/Views/ExerciseView.swift
@@ -25,53 +25,59 @@ public struct ExerciseView: View {
       Color(R.color.greyscale_01_171717)
         .ignoresSafeArea()
       
-      VStack {
-        HStack {
-          Button {
-            dismiss()
-          } label: {
-            Image(R.image.backButton)
+      if viewModel.state.isCountDownViewPresented {
+        CountDownView(isPresented: $viewModel.state.isCountDownViewPresented)
+      } else {
+        VStack {
+          HStack {
+            Button {
+              dismiss()
+            } label: {
+              Image(R.image.backButton)
+            }
+            Spacer()
+            Button {
+              viewModel.state.isRootViewPresented = true
+            } label: {
+              Image(systemName: "xmark")
+                .frame(width: 24, height: 24)
+            }
           }
+          .padding(.top, 16)
+          
           Spacer()
-          Button {
-            viewModel.state.isRootViewPresented = true
-          } label: {
-            Image(systemName: "xmark")
-              .frame(width: 24, height: 24)
+          
+          VStack(spacing: 10) {
+            Text("함께 운동한 시간")
+              .giantsFont(size: 22, weight: .regular, lineHeight: 22)
+              .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
+            
+            Text("00:00")
+              .giantsFont(size: 70, weight: .regular, lineHeight: 78)
+              .foregroundStyle(Color(R.color.white_FFFFFF))
+              .modifier(CenteredShearEffect(angle: .degrees(-15)))
           }
-        }
-        .padding(.top, 16)
-        
-        Spacer()
-        
-        VStack(spacing: 10) {
-          Text("함께 운동한 시간")
-            .giantsFont(size: 22, weight: .regular, lineHeight: 22)
-            .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
           
-          Text("00:00")
-            .giantsFont(size: 70, weight: .regular, lineHeight: 78)
-            .foregroundStyle(Color(R.color.white_FFFFFF))
-            .modifier(CenteredShearEffect(angle: .degrees(-15)))
-        }
-        
-        Spacer()
-        Spacer()
-        
-        Button {
+          Spacer()
+          Spacer()
           
-        } label: {
-          Text("시작")
-            .giantsFont(size: 24, weight: .regular, lineHeight: 28)
-            .foregroundStyle(Color(R.color.greyscale_02_252525))
-            .frame(width: 100, height: 100)
-            .background(Color(R.color.primary_01_D7FE63))
-            .clipShape(.rect(cornerRadius: 4))
+          Button {
+            withAnimation {
+              viewModel.state.isCountDownViewPresented = true
+            }
+          } label: {
+            Text("시작")
+              .giantsFont(size: 24, weight: .regular, lineHeight: 28)
+              .foregroundStyle(Color(R.color.greyscale_02_252525))
+              .frame(width: 100, height: 100)
+              .background(Color(R.color.primary_01_D7FE63))
+              .clipShape(.rect(cornerRadius: 4))
+          }
+          
+          Spacer()
         }
-        
-        Spacer()
+        .padding(.horizontal, 20)
       }
-      .padding(.horizontal, 20)
     }
     .navigationBarBackButtonHidden()
   }
