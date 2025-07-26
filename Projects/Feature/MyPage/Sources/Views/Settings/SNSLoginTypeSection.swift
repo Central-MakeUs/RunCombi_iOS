adc4db3234142b7626dfcd65ab44d13c4bb36e62
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-26 16:19:19 +0900

    feat($setting): SNS 로그인 타입 영역 UI 추가

---
+++ Projects/Feature/MyPage/Sources/Views/Settings/SNSLoginTypeSection.swift
@@ -0,0 +1,40 @@
+//
+//  SNSLoginTypeSection.swift
+//  FeatureMyPage
+//
+//  Created by 임경빈 on 7/26/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
+import SwiftUI
+
+import ResourceKit
+
+enum SNSType {
+  case kakao
+  case apple
+  
+  var symbol: Image {
+    switch self {
+    case .kakao:
+      Image(R.image.kakaoTypeIcon)
+    case .apple:
+      Image(R.image.appleTypeIcon)
+    }
+  }
+}
+
+struct SNSLoginTypeSection: View {
+  let type: SNSType
+  
+  var body: some View {
+    HStack {
+      Text("SNS 로그인")
+        .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+        .foregroundStyle(Color(R.color.ff_F4F4F4))
+      Spacer()
+      
+      type.symbol
+    }
+  }
+}
