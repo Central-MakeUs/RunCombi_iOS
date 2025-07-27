a185bd689eede92a3d50e2ddae8799f9d66d04e7
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-28 01:33:38 +0900

    feat($terms): 이용약관 동의 화면 노션 랜딩 추가

--- Projects/Feature/SignUp/Sources/Views/Agreement/ServiceAgreementRow.swift
+++ Projects/Feature/SignUp/Sources/Views/Agreement/ServiceAgreementRow.swift
@@ -9,22 +9,27 @@
 import SwiftUI
 
 import ResourceKit
+import SharedUtility
 import UserInterface
 
 struct ServiceAgreementRow: View {
-  let type: AgreementType
+  let type: TermsType
   let isSelected: Bool
   let action: () -> Void
   
   var body: some View {
-    Button {
-      action()
-    } label: {
-      HStack {
+    HStack {
+      NavigationLink {
+        NotionWebView(url: type.urlString)
+      } label: {
         Text(String(key: type.stringKey))
           .pretendardFont(size: 16, weight: .medium, lineHeight: 20)
           .foregroundStyle(Color(R.color.white_FFFFFF))
-        Spacer()
+      }
+      Spacer()
+      Button {
+        action()
+      } label: {
         if isSelected {
           Image(R.image.checkBox)
         } else {
