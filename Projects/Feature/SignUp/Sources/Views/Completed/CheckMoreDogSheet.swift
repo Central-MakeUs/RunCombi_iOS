6f111aaff74c74d1c40cb76cf53e745f94817d57
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-27 15:48:52 +0900

    fix($signUp): 회원가입 후 콤비 추가 시 마이페이지로 이동하는 로직 수정

--- Projects/Feature/SignUp/Sources/Views/Completed/CheckMoreDogSheet.swift
+++ Projects/Feature/SignUp/Sources/Views/Completed/CheckMoreDogSheet.swift
@@ -46,6 +46,7 @@ struct CheckMoreDogSheet: View {
         }
         
         Button {
+          userManager.shouldNavigateMyPage = true
           userManager.isLoggedIn = true
         } label :{
           PrimaryActionLabel(
--- Projects/Shared/Utility/Sources/User/UserManager.swift
+++ Projects/Shared/Utility/Sources/User/UserManager.swift
@@ -9,10 +9,12 @@
 import Foundation
 
 public class UserManager: ObservableObject {
-  @Published public var shouldRefresh: Bool = false
-  @Published public var isLoggedIn: Bool = false
-  @Published public var isSigning: Bool = false
-  @Published public var isAgreementChecked: Bool = false
+  @Published public var shouldRefresh: Bool = false // userManager 새로고침
+  @Published public var isLoggedIn: Bool = false // 메인 화면으로 이동
+  @Published public var isSigning: Bool = false // 로그인 후, 회원가입 화면으로 이동
+  @Published public var isAgreementChecked: Bool = false // 회원가입 중 서비스 동의 체크한 경우, 바로 정보 입력 화면으로 이동
+  @Published public var shouldNavigateMyPage: Bool = false // 회원가입 후 콤비 추가 시 마이페이지로 이동
+  
   @Published public var member: Member = .empty
   @Published public var petList: [Pet] = []
   
