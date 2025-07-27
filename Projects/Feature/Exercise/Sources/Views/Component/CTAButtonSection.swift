f0ef75841ab245834c72aaa03dc66c940e7e0207
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-27 21:46:20 +0900

    fix($exercise): 운동 시작 시 펫 리스트 하드코딩 제거

--- Projects/Feature/Exercise/Sources/Views/Component/CTAButtonSection.swift
+++ Projects/Feature/Exercise/Sources/Views/Component/CTAButtonSection.swift
@@ -9,8 +9,10 @@
 import SwiftUI
 
 import ResourceKit
+import SharedUtility
 
 struct CTAButtonSection: View {
+  @EnvironmentObject private var userManager: UserManager
   @ObservedObject var viewModel: ExerciseViewModel
   @Namespace private var ctaNamespace
   
@@ -19,7 +21,7 @@ struct CTAButtonSection: View {
       switch viewModel.state.exerciseStatus {
       case .ready:
         CTAButton(title: "시작", backgroundColor: Color(R.color.primary_01_D7FE63)) {
-          viewModel.send(action: .didTapStart)
+          viewModel.send(action: .didTapStart(userManager.petList.map { $0.petId }))
         }
       case .exercise:
         CTAButton(image: Image(R.image.pause), backgroundColor: Color(R.color.greyscale_02_252525)) {
