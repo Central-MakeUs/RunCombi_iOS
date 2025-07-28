4c08fce9f9946d781770f67c2a4d15d4d1d81131
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-28 19:07:15 +0900

    feat($record): Feature Record 추가

---
+++ Projects/Feature/Record/Sources/Views/RecordRootView.swift
@@ -0,0 +1,31 @@
+//
+//  RecordRootView.swift
+//  FeatureRecord
+//
+//  Created by Groonui on 7/28/25.
+//  Copyright © 2025 com.combo. All rights reserved.
+//
+
+import SwiftUI
+
+import ResourceKit
+
+public struct RecordRootView: View {
+  
+  public init() {}
+  
+  public var body: some View {
+    ZStack {
+      Color(R.color.greyscale_01_171717)
+        .ignoresSafeArea()
+      
+      VStack(spacing: 16) {
+        Spacer()
+      }
+    }
+  }
+}
+
+#Preview {
+  RecordRootView()
+}
