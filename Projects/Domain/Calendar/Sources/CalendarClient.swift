715cc23938776141d022f2f49fab8e150fb44718
Gyeongbin Im <hsmikvyv@gmail.com>
2025-07-31 22:34:25 +0900

    feat($detail): 운동 상세 정보 API 통신 로직 구현

--- Projects/Domain/Calendar/Sources/CalendarClient.swift
+++ Projects/Domain/Calendar/Sources/CalendarClient.swift
@@ -16,6 +16,7 @@ import SharedUtility
 public protocol CalendarClientProtocol {
   func fetchMonthData(token: String, year: Int, month: Int) async throws -> MonthDataResult
   func fetchDayData(token: String, year: Int, month: Int, day: Int) async throws -> [DayDataResult]
+  func fetchRunDetail(token: String, runID: Int) async throws -> RunDetail
 }
 
 public final class CalendarClient: CalendarClientProtocol {
@@ -64,6 +65,31 @@ public final class CalendarClient: CalendarClientProtocol {
     Logger.d("DayData response: \(response)")
     if response.code == "STATUS200", let result = response.result {
       return result.map { $0.toEntity() }
+    } else {
+      throw ServerError.serverError
+    }
+  }
+  
+  public func fetchRunDetail(token: String, runID: Int) async throws -> RunDetail {
+    let headers: HTTPHeaders = [
+      "Authorization": "Bearer \(token)"
+    ]
+
+    let jsonDict: [String: Any] = ["runId": runID]
+    let jsonData = try JSONSerialization.data(withJSONObject: jsonDict)
+
+    let response = try await Networking.shared.sendRequestWithRaw(
+      "/api/calender/getRunData",
+      resultType: ResultModel<RunDetailModel>.self,
+      method: .post,
+      rawBody: jsonData,
+      headers: headers
+    )
+
+    Logger.d("fetchRunData response: \(response)")
+
+    if response.code == "STATUS200", let result = response.result {
+      return result.toEntity()
     } else {
       throw ServerError.serverError
     }
