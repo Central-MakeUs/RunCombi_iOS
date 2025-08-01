fd4f04d7eebe10b75ec9164e972770c40b6729f8
Gyeongbin Im <hsmikvyv@gmail.com>
2025-08-02 03:38:52 +0900

    feat($record): run image, route image가 동시에 존재할 때 나타나는 헤더 이미지 영역 UI 구현

--- Projects/Feature/Record/Sources/Views/Section/RecordDetailHeader.swift
+++ Projects/Feature/Record/Sources/Views/Section/RecordDetailHeader.swift
@@ -20,22 +20,52 @@ struct RecordDetailHeader: View {
   @Environment(\.dismiss) var dismiss
   @Binding var runDetail: RunDetail
   @Binding var isMenuPresented: Bool
+  @Binding var selectedImageData: Data?
   
+  @State private var currentTab: Int = 1
   @State private var routeImageURL = ""
   @State private var runImageURL = ""
   var hasRunImage: Bool {
     URL(string: runImageURL) != nil || selectedImageData != nil
   }
+  
   @State private var isPhotosPickerPresented: Bool = false
   @State private var selectedPicture: PhotosPickerItem?
-  @Binding var selectedImageData: Data?
   
   var body: some View {
     ZStack(alignment: .top) {
       Color(R.color.greyscale_02_252525)
       
       if let routeImageUrl = URL(string: routeImageURL), hasRunImage {
-        // TODO: - 이미지 두 장 UI
+        TabView(selection: $currentTab) {
+          Group {
+            if let runImageURL = URL(string: runImageURL) {
+              KFImage(runImageURL)
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            } else if let selectedImageData, let uiImage = UIImage(data: selectedImageData) {
+              Image(uiImage: uiImage)
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+          }
+          .tag(1)
+          
+          KFImage(routeImageUrl)
+            .resizable()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .tag(2)
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .overlay(alignment: .bottomTrailing) {
+          Text("\(currentTab) / 2")
+            .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+            .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
+            .padding(EdgeInsets(top: 2, leading: 6, bottom: 2, trailing: 6))
+            .background(Color(R.color.greyscale_03_333333).opacity(0.8))
+            .clipShape(.rect(cornerRadius: 2))
+            .padding(18)
+        }
       } else if let routeImageUrl = URL(string: routeImageURL) {
         KFImage(routeImageUrl)
           .resizable()
