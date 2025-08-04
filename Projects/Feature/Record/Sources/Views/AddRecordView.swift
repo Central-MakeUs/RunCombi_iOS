b4e03d13407ca80bef6e130a2b94c4cda7e7c589
Gyeongbin Im <hsmikvyv@gmail.com>
2025-08-05 03:03:06 +0900

    fix($record): 기록 추가 화면의 초기 시작 일자가 선택한 날짜로 설정되도록 변경

--- Projects/Feature/Record/Sources/Views/AddRecordView.swift
+++ Projects/Feature/Record/Sources/Views/AddRecordView.swift
@@ -36,148 +36,152 @@ public struct AddRecordView: View {
     selectedPets.isEmpty
   }
   
+  @Binding var selectedDate: Date?
   let refreshAction: () -> Void
   
-  public init(refreshAction: @escaping () -> Void) {
+  public init(of selectedDate: Binding<Date?>, refreshAction: @escaping () -> Void) {
+    self._selectedDate = selectedDate
     self.refreshAction = refreshAction
   }
   
   public var body: some View {
-    VStack(spacing: 0) {
-      EditHeader(title: "기록 추가", isDisabled: isDisabled)
-      {
-        isCancelSheetPresented = true
-      } saveAction: {
-        addRecord()
-      }
-      
-      ScrollView {
-        VStack(spacing: 24) {
-          VStack(spacing: 12) {
-            Button {
-              isPickerPresented = true
-            } label: {
-              HStack {
-                Text("시작 일시")
-                  .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
-                  .foregroundStyle(Color(R.color.greyscale_06_999999))
-                
-                Spacer()
-                
-                HStack(spacing: 9) {
-                  Text(DateFormatter.yyyyMMddwitDdot.string(from: startDate))
-                    .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
-                    .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
-                  
-                  Text(DateFormatter.HHmm.string(from: startDate))
-                    .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
-                    .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
-                }
-              }
-              .frame(height: 44)
-              .padding(.horizontal, 12)
-              .background(Color(R.color.greyscale_03_333333))
-              .clipShape(.rect(cornerRadius: 6))
-            }
-            
-            HStack(spacing: 12) {
-              HStack {
-                Text("거리")
-                  .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
-                  .foregroundStyle(Color(R.color.greyscale_06_999999))
-                
-                Spacer()
-                
-                HStack(spacing: 4) {
-                  TextField(
-                    "",
-                    text: $typpedDistance,
-                    prompt: Text("0.00")
-                      .foregroundStyle(Color(R.color.greyscale_06_999999))
-                  )
-                  .keyboardType(.decimalPad)
-                  .multilineTextAlignment(.trailing)
-                  .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
-                  .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
-                  .onChange(of: typpedDistance) { oldValue, newValue in
-                    
-                  }
-                  
-                  Text("km")
-                    .foregroundStyle(Color(R.color.greyscale_06_999999))
-                    .pretendardFont(size: 14, weight: .medium, lineHeight: 24)
-                }
-              }
-              .frame(height: 44)
-              .padding(.horizontal, 12)
-              .background(Color(R.color.greyscale_03_333333))
-              .clipShape(.rect(cornerRadius: 6))
-              
-              
-              HStack {
-                Text("시간")
-                  .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
-                  .foregroundStyle(Color(R.color.greyscale_06_999999))
-                
-                Spacer()
-                
-                HStack(spacing: 4) {
-                  TextField(
-                    "",
-                    text: $typpedTime,
-                    prompt: Text("0")
-                      .foregroundStyle(Color(R.color.greyscale_06_999999))
-                  )
-                  .keyboardType(.numberPad)
-                  .multilineTextAlignment(.trailing)
-                  .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
-                  .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
-                  .onChange(of: typpedTime) { oldValue, newValue in
-                    if let time = Int(typpedTime), time > 999 {
-                      typpedTime = oldValue
-                    }
-                  }
-                  
-                  Text("min")
-                    .foregroundStyle(Color(R.color.greyscale_06_999999))
-                    .pretendardFont(size: 14, weight: .medium, lineHeight: 24)
-                }
-              }
-              .frame(height: 44)
-              .padding(.horizontal, 12)
-              .background(Color(R.color.greyscale_03_333333))
-              .clipShape(.rect(cornerRadius: 6))
-            }
-          }
-          .padding(.top, 32)
-          
-          VStack(spacing: 16) {
-            Text("함께한 콤비는")
-              .pretendardFont(size: 18, weight: .semiBold, lineHeight: 30)
-              .foregroundStyle(Color(R.color.white_FFFFFF))
-              .frame(maxWidth: .infinity, alignment: .leading)
-            
-            SelectDogView(selectedPets: $selectedPets)
-          }
-          
-          VStack(alignment: .leading, spacing: 20) {
-            Text("이번 운동은")
-              .pretendardFont(size: 18, weight: .semiBold, lineHeight: 30)
-              .foregroundStyle(Color(R.color.white_FFFFFF))
-            
+    NavigationStack {
+      VStack(spacing: 0) {
+        EditHeader(title: "기록 추가", isDisabled: isDisabled)
+        {
+          isCancelSheetPresented = true
+        } saveAction: {
+          addRecord()
+        }
+        
+        ScrollView {
+          VStack(spacing: 24) {
             VStack(spacing: 12) {
-              ForEach(WalkStyleType.allCases.reversed(), id: \.self) { type in
-                if type != .none {
-                  Button {
-                    selectedMemberWalkStyle = type
-                  } label: {
-                    Text(type.memberRunStyle)
-                      .pretendardFont(size: 18, weight: .semiBold, lineHeight: 30)
-                      .foregroundStyle(selectedMemberWalkStyle == type ? Color(R.color.greyscale_03_333333):  Color(R.color.greyscale_08_EDEDED))
-                      .padding(EdgeInsets(top: 9, leading: 12, bottom: 9, trailing: 12))
-                      .frame(maxWidth: .infinity)
-                      .background(selectedMemberWalkStyle == type ? Color(R.color.primary_01_D7FE63):  Color(R.color.greyscale_04_525252))
-                      .clipShape(.rect(cornerRadius: 6))
+              Button {
+                isPickerPresented = true
+              } label: {
+                HStack {
+                  Text("시작 일시")
+                    .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+                    .foregroundStyle(Color(R.color.greyscale_06_999999))
+                  
+                  Spacer()
+                  
+                  HStack(spacing: 9) {
+                    Text(DateFormatter.yyyyMMddwitDdot.string(from: startDate))
+                      .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
+                      .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+                    
+                    Text(DateFormatter.HHmm.string(from: startDate))
+                      .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
+                      .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+                  }
+                }
+                .frame(height: 44)
+                .padding(.horizontal, 12)
+                .background(Color(R.color.greyscale_03_333333))
+                .clipShape(.rect(cornerRadius: 6))
+              }
+              
+              HStack(spacing: 12) {
+                HStack {
+                  Text("거리")
+                    .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+                    .foregroundStyle(Color(R.color.greyscale_06_999999))
+                  
+                  Spacer()
+                  
+                  HStack(spacing: 4) {
+                    TextField(
+                      "",
+                      text: $typpedDistance,
+                      prompt: Text("0.00")
+                        .foregroundStyle(Color(R.color.greyscale_06_999999))
+                    )
+                    .keyboardType(.decimalPad)
+                    .multilineTextAlignment(.trailing)
+                    .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
+                    .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+                    .onChange(of: typpedDistance) { oldValue, newValue in
+                      
+                    }
+                    
+                    Text("km")
+                      .foregroundStyle(Color(R.color.greyscale_06_999999))
+                      .pretendardFont(size: 14, weight: .medium, lineHeight: 24)
+                  }
+                }
+                .frame(height: 44)
+                .padding(.horizontal, 12)
+                .background(Color(R.color.greyscale_03_333333))
+                .clipShape(.rect(cornerRadius: 6))
+                
+                
+                HStack {
+                  Text("시간")
+                    .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+                    .foregroundStyle(Color(R.color.greyscale_06_999999))
+                  
+                  Spacer()
+                  
+                  HStack(spacing: 4) {
+                    TextField(
+                      "",
+                      text: $typpedTime,
+                      prompt: Text("0")
+                        .foregroundStyle(Color(R.color.greyscale_06_999999))
+                    )
+                    .keyboardType(.numberPad)
+                    .multilineTextAlignment(.trailing)
+                    .foregroundStyle(Color(R.color.greyscale_08_EDEDED))
+                    .pretendardFont(size: 16, weight: .medium, lineHeight: 26)
+                    .onChange(of: typpedTime) { oldValue, newValue in
+                      if let time = Int(typpedTime), time > 999 {
+                        typpedTime = oldValue
+                      }
+                    }
+                    
+                    Text("min")
+                      .foregroundStyle(Color(R.color.greyscale_06_999999))
+                      .pretendardFont(size: 14, weight: .medium, lineHeight: 24)
+                  }
+                }
+                .frame(height: 44)
+                .padding(.horizontal, 12)
+                .background(Color(R.color.greyscale_03_333333))
+                .clipShape(.rect(cornerRadius: 6))
+              }
+            }
+            .padding(.top, 32)
+            
+            VStack(spacing: 16) {
+              Text("함께한 콤비는")
+                .pretendardFont(size: 18, weight: .semiBold, lineHeight: 30)
+                .foregroundStyle(Color(R.color.white_FFFFFF))
+                .frame(maxWidth: .infinity, alignment: .leading)
+              
+              SelectDogView(selectedPets: $selectedPets)
+            }
+            
+            VStack(alignment: .leading, spacing: 20) {
+              Text("이번 운동은")
+                .pretendardFont(size: 18, weight: .semiBold, lineHeight: 30)
+                .foregroundStyle(Color(R.color.white_FFFFFF))
+              
+              VStack(spacing: 12) {
+                ForEach(WalkStyleType.allCases.reversed(), id: \.self) { type in
+                  if type != .none {
+                    Button {
+                      selectedMemberWalkStyle = type
+                    } label: {
+                      Text(type.memberRunStyle)
+                        .pretendardFont(size: 18, weight: .semiBold, lineHeight: 30)
+                        .foregroundStyle(selectedMemberWalkStyle == type ? Color(R.color.greyscale_03_333333):  Color(R.color.greyscale_08_EDEDED))
+                        .padding(EdgeInsets(top: 9, leading: 12, bottom: 9, trailing: 12))
+                        .frame(maxWidth: .infinity)
+                        .background(selectedMemberWalkStyle == type ? Color(R.color.primary_01_D7FE63):  Color(R.color.greyscale_04_525252))
+                        .clipShape(.rect(cornerRadius: 6))
+                    }
                   }
                 }
               }
@@ -185,22 +189,24 @@ public struct AddRecordView: View {
           }
         }
       }
+      .padding(.horizontal, 20)
+      .padding(.bottom, 20)
+      .frame(maxWidth: .infinity)
+      .background(Color(R.color.greyscale_01_171717))
+      .navigationDestination(isPresented: $isRecordDetailViewPresented) {
+        RecordDetailView(of: 0, snackBarItem: .constant("")) // TODO: - 하드 코딩
+      }
     }
-    .padding(.horizontal, 20)
-    .padding(.bottom, 20)
-    .frame(maxWidth: .infinity)
-    .background(Color(R.color.greyscale_01_171717))
     .onAppear {
       UIApplication.shared.hideKeyboard()
+      print(selectedDate)
+      startDate = (selectedDate).ifNil(then: Date())
     }
     .bottomSheet(isPresented: $isPickerPresented) {
       RecordDatePickerSheet(isPresented: $isPickerPresented, startDate: $startDate)
     }
     .bottomSheet(isPresented: $isCancelSheetPresented) {
       CancelRecordBottomSheet(isPresented: $isCancelSheetPresented)
-    }
-    .navigationDestination(isPresented: $isRecordDetailViewPresented) {
-      RecordDetailView(of: 0, snackBarItem: .constant(""))
     }
   }
   
@@ -218,7 +224,8 @@ public struct AddRecordView: View {
           }
         ))
         refreshAction()
-        isRecordDetailViewPresented = true
+//        isRecordDetailViewPresented = true
+        dismiss()
       } catch {
         Logger.e("\(error)")
       }
