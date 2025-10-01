d65a5a4f6baf5d9365cfd026c489e03f1e3363b6
Gyeongbin Im <hsmikvyv@gmail.com>
2025-10-02 00:44:35 +0900

    chore: build version update 1.1.0(21)

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -19,8 +19,8 @@ let infoPlist: InfoPlist = .extendingDefault(
   with: [
     "CFBundleDisplayName": "\(AppEnvironment.appName)",
     "CFBundleIconName": "AppIcon",
-    "CFBundleShortVersionString": "1.0.4",
-    "CFBundleVersion": "20",
+    "CFBundleShortVersionString": "1.1.0",
+    "CFBundleVersion": "21",
     "UILaunchScreen": [
       "UIColorName": "launchBackgroundColor",
       "UIImageName": ""
